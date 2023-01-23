a26f87ac55e701217e67c0954b49fa67c4df8702
danilneflix <115938025+danilneflix@users.noreply.github.com>
2023-01-23 09:51:10 +0700

    Add files via upload

---
+++ Новая папка (5)/scripts/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('#preloader').fadeOut(400);
+});
+
+document.getElementById('btn').onclick = function () {
+    document.getElementById('menu').classList.toggle('visible');
+}
+
+
+
